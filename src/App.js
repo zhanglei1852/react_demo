f98a318cba40e66bfa40d88cde860b532776804c
zhanglei <zhanglei@shengdun.com>
2018-10-19 17:38:35 +0800

    6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
-
+import React, { Component, Fragment } from 'react'
+import './style.css'
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      show: true
+    }
+    this.toggleClass = this.toggleClass.bind(this)
+  }
+
+  toggleClass () {
+    this.setState(() => ({
+      show: !this.state.show
+    }))
+  }
+
   render() {
     return (
-      <div>
-        hello world
-      </div>
+      <Fragment>
+        <div className = {this.state.show ? 'show' : 'hide'}>
+          hello world
+        </div>
+        <button onClick={ this.toggleClass }>toggleClass</button>
+      </Fragment>
     );
   }
 }
---
+++ src/redux.js
@@ -0,0 +1,8 @@
+/**
+ * Redux
+ * state => 普通JavaScript对象、也可以是基础类型
+ * reducer => reducer接受一个state和action 并返回新的state 的函数
+ * 
+ * 
+ * 
+ */
